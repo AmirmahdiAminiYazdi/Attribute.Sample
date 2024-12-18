9bc765b2068a2c1c7119544da62de6e1f5e60c8a
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2024-12-18 11:15:28 +0330

    initial Sample Project

---
+++ Attribute.UI/Program.cs
@@ -0,0 +1,9 @@
+﻿Person AmirMahdi = new()
+{
+    FirstName ="Amir Mahdi ",
+    LastName = "Amini Yazdi",
+    Age = 28
+};
+PersonPrinter p1 = new(AmirMahdi);
+p1.print();
+Console.ReadLine();
---
+++ AttributeSample.Domain/Person.cs
@@ -0,0 +1,33 @@
+﻿
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
+
+public class PersonPrinter
+{
+    private readonly Person _person;
+
+    public PersonPrinter(Person person)
+    {
+        _person = person;
+    }
+
+    public void print()
+    {
+        PrintFullName();
+        PrintAge();
+    }
+
+    private void PrintFullName()
+    {
+        Console.WriteLine($"FullName:{_person.FirstName},{_person.LastName}");
+    }
+
+    private void PrintAge()
+    {
+        Console.WriteLine($"Age : {_person.Age}");
+    }
+}
