9decb84d3433a23bc638a964a81b02630208393e
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2024-12-22 11:52:04 +0330

    intial  Data Validation

--- AttributeSample.Domain/Person.cs
+++ AttributeSample.Domain/Person.cs
@@ -1,11 +1,14 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 
 [DebuggerDisplay("Person: {FirstName} {LastName}, Age: {Age}")]
 [DebuggerTypeProxy(typeof(PersonDebuggerTypeProxy))]
 public class Person
 {
     [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    [Required]
     public string FirstName { get; set; }
+    [StringLength(100)]
     public string LastName { get; set; }
     public int Age { get; set; }
 
