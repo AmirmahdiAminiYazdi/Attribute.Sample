79a00d88209d5afb7408b553f5395f50998c67a2
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2024-12-24 12:13:51 +0330

    initial Reflection & DiscoveringMetaData

---
+++ AttributeSample.DiscoveringMetaData/MetaDataPrinter.cs
@@ -0,0 +1,72 @@
+﻿namespace AttributeSample.DiscoveringMetaData
+{
+    public class MetaDataPrinter
+    {
+        private readonly Type _type;
+
+        public MetaDataPrinter(Type type)
+        {
+            _type = type;
+        }
+        public void Print()
+        {
+            PrintMainInfo();
+            PrintMethodInfo();
+            PrintPropertyInfo();
+            PrintFieldInfo();
+           
+
+        }
+
+        private void PrintFieldInfo()
+        {
+            Console.WriteLine($"************ Fields information of type {_type.Name} ************");
+            var fieldInfos = _type.GetFields();
+            foreach (var fieldInfo in fieldInfos)
+            {
+                Console.WriteLine($"{fieldInfo.FieldType} {fieldInfo.Name}");
+            }
+        }
+
+        private void PrintPropertyInfo()
+        {
+
+            Console.WriteLine($"************ Properties information of type {_type.Name} ************");
+            var propetyInfos = _type.GetProperties();
+            foreach (var propetyInfo in propetyInfos)
+            {
+                Console.WriteLine($"{propetyInfo.PropertyType} {propetyInfo.Name}");
+            }
+        }
+
+        private void PrintMethodInfo()
+        {
+            Console.WriteLine($"************ Methods information of type {_type.Name} ************");
+            var methodInfo = _type.GetMethods();
+            foreach (var method in methodInfo)
+            {
+                Console.Write($"{method.Name}(");
+                var parameters = method.GetParameters();
+                foreach (var param in parameters)
+                {
+                    Console.Write($"{param.ParameterType} {param.Name},");
+                }
+
+
+                Console.WriteLine(")");
+            }
+        }
+
+        private void PrintMainInfo()
+        {
+            Console.WriteLine($"************ Information of type {_type.Name} ************");
+            Console.WriteLine($"FullName: {_type.FullName}");
+            Console.WriteLine($"AssemblyQualifiedName: {_type.AssemblyQualifiedName}");
+            Console.WriteLine($"BaseType: {_type.BaseType}");
+            Console.WriteLine($"Namespace: {_type.Namespace}");
+            Console.WriteLine($"IsAbstract: {_type.IsAbstract}");
+            Console.WriteLine($"IsEnum: {_type.IsEnum}");
+            Console.WriteLine($"IsNotPublic: {_type.IsNotPublic}");
+        }
+    }
+}
---
+++ AttributeSample.DiscoveringMetaData/Program.cs
@@ -0,0 +1,16 @@
+﻿using AttributeSample.DiscoveringMetaData;
+using AttributeSample.Domain;
+
+var intPrinter = new MetaDataPrinter(typeof(int));
+var personPrinter = new MetaDataPrinter(typeof(Person));
+
+intPrinter.Print();
+Console.WriteLine("Press any key to print INT MetaData");
+Console.ReadKey();
+Console.Clear();
+
+personPrinter.Print();
+
+Console.WriteLine("Press any key to print Person MetaData");
+Console.ReadKey();
+Console.Clear();
