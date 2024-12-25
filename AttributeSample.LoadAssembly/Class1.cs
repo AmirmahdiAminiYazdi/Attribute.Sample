438ca8940bff5d8afbd939d198519bb4e9bb3abd
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2024-12-25 11:31:54 +0330

    initial AttributeSample.LoadAssembly

---
+++ AttributeSample.LoadAssembly/Class1.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Reflection;
+
+var assembly = Assembly.LoadFrom(@"D:\Users\mashadservice\source\repos\Attribute.Sample\AttributeSample.Domain\bin\Debug\net8.0\AttributeSample.Domain.dll");    
+
+var types = assembly.GetTypes();
+Console.WriteLine($"********* {assembly.FullName}*********");
+foreach (var type in types)
+{
+    Console.WriteLine($"Type's name :{type.Name}");
+}
+Console.ReadLine();
